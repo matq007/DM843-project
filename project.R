699895cc63f141589339ee05ac4ad2d8dd7e2e57
Martin Proks <mproksik@gmail.com>
2018-04-05 11:42:52 +0200

    Correct for 0

--- project.R
+++ project.R
@@ -3,3 +3,11 @@ my.dataset <- read.table("all-vs-all.tsv", header = FALSE, sep = "\t")
 
 my.data <- data.frame(my.dataset$V1, my.dataset$V2, my.dataset$V11)
 colnames(my.data) <- c("protein1", "protein2", "E-value")
+
+# Correct for 0 
+tmp_small <- my.data$`E-value`
+corrected <- replace(tmp_small, tmp_small == 0, 1e-200)
+my.data <- cbind(my.data, corrected)
+colnames(my.data) <- c("protein1", "protein2", "E-value", "corrected")
+
+# Build similarity matrix

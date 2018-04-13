af572826bdc1e038fa23f7d0f68e61c5d21b646a
iustin94 <o.iustin@gmail.com>
2018-04-13 09:06:17 +0200

    Added f-score function.Works for numbers, need to adjust for labels

--- project.R
+++ project.R
@@ -51,6 +51,26 @@ colnames(family) <- c("protein", "class")
 #Compare results to golden standard on proteins we have 
 comparison <- merge(gold.standard, family, by="protein")
 
+measurePrecisionRecall <- function(predict, actual_labels){
+  precision <- sum(predict & actual_labels) / sum(predict)
+  recall <- sum(predict & actual_labels) / sum(actual_labels)
+  fmeasure <- 2 * precision * recall / (precision + recall)
+  
+  cat('precision:  ')
+  cat(precision * 100)
+  cat('%')
+  cat('\n')
+  
+  cat('recall:     ')
+  cat(recall * 100)
+  cat('%')
+  cat('\n')
+  
+  cat('f-measure:  ')
+  cat(fmeasure * 100)
+  cat('%')
+  cat('\n')
+}
 cluster.stats(family, gold.standard)
 
 #Spectral Clust

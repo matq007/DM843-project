08d887061272ce0448ed7f44d931f8ed804cf3a2
Martin Proks <mproksik@gmail.com>
2018-04-15 19:48:05 +0200

    Updated version

---
+++ functions.R
@@ -0,0 +1,155 @@
+similarity_matrix <- function (p_data, p_proteins) {
+  similarity_n = length(p_proteins)
+  similarity <- matrix(max(p_data$corrected), nrow = similarity_n, ncol = similarity_n)
+  colnames(similarity) <- p_proteins
+  rownames(similarity) <- p_proteins
+  
+  for (i in 1:nrow(p_data)) {
+    index <- p_data[i, ]
+    tmp_i = as.character(index$protein1)
+    tmp_j = as.character(index$protein2)
+    similarity[tmp_i, tmp_j] <- min(index$corrected, similarity[tmp_i, tmp_j])
+  }
+  
+  return (similarity)
+}
+
+method_1 <- function(p_similarity, p_proteins, p_length) {
+  
+  p_similarity <- -log(p_similarity)
+  clusters <- NULL
+  clusters[1] <- 0
+  for (i in 2:p_length) {
+    clusters[i] <- kmeans(p_similarity, i, nstart = 10)$tot.withinss
+  }
+  
+  # Find best 
+  k <- which.max(clusters)
+  print(clusters)
+  print(c("Best fitting K ", k))
+  clusters <- kmeans(p_similarity, k, nstart = 10)
+  
+  family <- NULL
+  family <- cbind(p_proteins)
+  family <- cbind(family, clusters$clusters)
+  family <- as.data.frame(family)
+  colnames(family) <- c("protein", "class")
+  return (family)
+}
+
+method_2 <- function (p_k, p_similarity, p_proteins) {
+  
+  p_similarity <- -log(p_similarity)
+  dij <- dist(scale(p_similarity, center = TRUE, scale = TRUE))
+  clust <- hclust(dij, method = "average")
+  
+  clusters <- cutree(clust, p_k)
+  print(c("Clusters: ", max(as.numeric(clusters))))
+  
+  family <- NULL
+  family <- cbind(p_proteins)
+  family <- cbind(family, as.numeric(clusters))
+  family <- as.data.frame(family)
+  colnames(family) <- c("protein", "class")
+  
+  return (family)
+}
+
+method_3 <- function (p_similarity, p_proteins) {
+  
+  p_similarity <- as.matrix(p_similarity)
+  p_similarity <- round(p_similarity)
+  p_similarity <- replace(p_similarity, p_similarity <= 0, 1)
+  p_similarity <- replace(p_similarity, p_similarity != 1, 0)
+  
+  adjacency <- graph.adjacency(p_similarity, diag = TRUE)
+  # plot(adjacency)
+  
+  hmm <- mcl(x = p_similarity, addLoops = TRUE, ESM = TRUE, allow1 = TRUE, expansion = 1, inflation = 1)
+  print(c("Clusters: ", as.numeric(hmm$K)))
+  print(c("Iterations: ", as.numeric(hmm$n.iterations)))
+  
+  family <- NULL
+  family <- cbind(p_proteins)
+  family <- cbind(family, hmm$Cluster)
+  family <- as.data.frame(family)
+  colnames(family) <- c("protein", "class")
+  
+  return (family)
+}
+
+#####################################################################################################################
+# VALIDATION
+
+# Fraction of predictions that are relevant 
+# (hits that are clustered in the same cluster)/(all hits)
+normalizeTable <- function(predictions, standard){
+  
+  #for each protein family we know in the standard
+  for(f in unique(standard$class)){
+    #get the proteins
+    proteins <- standard[standard$class == f,]
+    
+    # get the predictions for the protein family
+    proteins.clust <- predictions[(predictions$protein %in% proteins$protein), ] 
+    
+    # get the most occouring cluster in prediction familty
+    biggest.cluster <- tail(names(sort(table(proteins.clust$class))), 1)
+    
+    #Mark the small clusters as beeing wrong in the predict
+    predictions$class[(predictions$protein %in% proteins$protein) & (predictions$class != biggest.cluster)] <- NA
+    
+    #Correct the label for the biggest one to match the gold standard, the rest assume are wron
+    predictions$class[(predictions$protein %in% proteins$protein) & !is.na(predictions$class) & (predictions$class == biggest.cluster)] <- proteins$class[1]
+    
+  }
+  
+  return (c(predictions, standard))
+} 
+
+precision <- function(predictions, standard){
+  
+  normalize <- normalizeTable(predictions, standard)
+  predictions <- normalize[2]$class
+  standard <- normalize[4]$class
+  
+  assumed.positive <- sum(!is.na(predictions))
+  total.predictions <- length(predictions)
+  
+  return(as.double(assumed.positive / total.predictions))
+  #return (1)
+}
+
+#Fraction of relevant instances that are retrieved
+recall <- function(predictions, standard){
+  
+  assumed.positive <- sum(!is.na(predictions$class))
+  total.true <- length(standard$class)
+  
+  return(as.double(assumed.positive / total.true))
+}
+
+f.score <- function(predictions,standard){
+  
+  normalize <- normalizeTable(predictions,standard)[1]
+  predictions <- normalize[1]
+  standard <- normalize[2]
+  
+  prec <- precision(predictions, standard)
+  rec <- recall(predictions, standard) 
+  
+  return (2*prec*rec/prec+rec)
+}
+
+calculate_best_score <- function(p_test_range, p_gs, p_similarity, p_proteins, p_method) {
+  
+  result <- NULL
+  
+  for (i in p_test_range) {
+    family <- p_method(i, p_similarity, p_proteins)
+    tmp <- family[family$protein %in% p_gs$protein, ]
+    result[i] <- precision(tmp, p_gs)
+  }
+  
+  return (result)
+}
--- project.R
+++ project.R
@@ -1,182 +1,63 @@
+# Mini project - Unsupervised learning
+library("igraph")
+library("MCL")
 
-# install.package("fpc")
+source("functions.R")
 
-# READ GOLD STANDARD
-gold.standard <- read.table("gold_standard.txt", header = FALSE, sep = "\t")
-colnames(gold.standard)<- c("protein","family")
+#####################################################################################################################
+# DATA
 
-# READ FILE
-my.dataset <- read.table("all-vs-all.tsv", header = FALSE, sep = "\t")
-my.gs <- read.table("gold_standard.txt", header = FALSE, sep = "\t")
+my.dataset <- read.table("data/all-vs-all.tsv", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+my.data <- data.frame(my.dataset$V1, my.dataset$V2, my.dataset$V11)
+colnames(my.data) <- c("protein1", "protein2", "E-value")
+
+proteins <- as.matrix(read.table("data/all_proteins_names.txt", header = FALSE, stringsAsFactors = FALSE))
+
+my.gs <- read.table("data/gold_standard.txt", header = FALSE, sep = "\t")
 my.gs <- cbind(my.gs, unlist(lapply(my.gs$V2, function (x) return (unlist(strsplit(as.character(x), "_"))[2]))))
 my.gs <- my.gs[,-2]
 colnames(my.gs) <- c("protein", "class")
 
-my.data <- data.frame(my.dataset$V1, my.dataset$V2, my.dataset$V11)
-colnames(my.data) <- c("protein1", "protein2", "E-value")
-
-# Correct for 0 
+# Correction for 0
+# Every 0 is replaced with 1e-200, which we consider as the highest significance
 tmp_small <- my.data$`E-value`
 corrected <- replace(tmp_small, tmp_small == 0, 1e-200)
 my.data <- cbind(my.data, corrected)
 colnames(my.data) <- c("protein1", "protein2", "E-value", "corrected")
 
-# Build similarity matrix
-proteins <- as.matrix(read.table("all_proteins_names.txt", header = FALSE))
-similarity_n <- length(proteins)
-similarity <- matrix(max(my.data$corrected), nrow = similarity_n, ncol = similarity_n)
-colnames(similarity) <- proteins
-rownames(similarity) <- proteins
+#####################################################################################################################
+# BUILD SIMILARITY MATRIX
+similarity <- similarity_matrix(my.data, proteins)
 
-for (i in 1:nrow(my.data)) {
-  index <- my.data[i, ]
-  tmp_i = as.character(index$protein1)
-  tmp_j = as.character(index$protein2)
-  similarity[tmp_i, tmp_j] <- min(index$corrected, similarity[tmp_i, tmp_j])
-}
+#####################################################################################################################
+# CLUSTERING METHODS
 
-similarity <- -log(similarity)
+my.result <- NULL
 
-# Clustering algorithms
-#save.image("workspace.RData")
-# load("workspace.RData")
-# plot(c(similarity))
+# K-MEANS
+set.seed(2)
+my.result.kmeans <- method_1(similarity, length(proteins))
 
-dij <- dist(scale(similarity, center = TRUE, scale = TRUE))
-clust <- hclust(dij, method = "average")
-family <- NULL
-family <- cbind(proteins)
-family <- cbind(family, cutree(clust, 30))
-family <- as.data.frame(family)
-colnames(family) <- c("protein", "class")
+# HIERARCHICAL CLUSTERING
+my.result.hclust <- method_2(300, similarity, proteins)
 
-#Make clusters groups from gold standard 
+# HIDDEN MARKOV MODEL
+my.result.hmm <- method_3(similarity, proteins)
 
-#Compare results to golden standard on proteins we have 
-comparison <- merge(gold.standard, family, by="protein")
+#####################################################################################################################
+# VALIDATION
+# Make clusters groups from gold standard 
+# Compare results to partial gold standard on the proteins we have 
 
-measurePrecisionRecall <- function(predict, actual_labels){
-  precision <- sum(predict & actual_labels) / sum(predict)
-  recall <- sum(predict & actual_labels) / sum(actual_labels)
-  fmeasure <- 2 * precision * recall / (precision + recall)
-  
-  cat('precision:  ')
-  cat(precision * 100)
-  cat('%')
-  cat('\n')
-  
-  cat('recall:     ')
-  cat(recall * 100)
-  cat('%')
-  cat('\n')
-  
-  cat('f-measure:  ')
-  cat(fmeasure * 100)
-  cat('%')
-  cat('\n')
-}
-cluster.stats(family, gold.standard)
+#test_range <- 1:length(proteins)
+test_range <- 100:101
+my.result.kmeans <- calculate_best_score(test_range, my.gs, similarity, proteins, method_1)
+my.result.hclust <- calculate_best_score(test_range, my.gs, similarity, proteins, method_2)
+my.result.hmm <- calculate_best_score(test_range, my.gs, similarity, proteins, method_3)
 
-# k means clustering
-set.seed(123)
-results <- kmeans(similarity, 550)
-table(family$class, results$cluster)
-# run multiple time to extract similar clusters
-bestfi <- NULL
-for (i in 1:100) {
-  set.seed(123)
-  results <- kmeans(similarity, 550)
-  table(family$class, results$cluster)
-  if(family$class[i] == results$cluster[i])
-  {
-    bestfit <- table(family$class, results$cluster)
-  }
-}
-# function to compute total within-cluster sum of square 
-wss <- function(k) {
-  kmeans(similarity, k, nstart = 10 )$tot.withinss
-}
-# Compute and plot wss for k = 1 to k = 15
-k.values <- 1:550
-
-# extract wss for 2-550 clusters
-library(factoextra) #optimal number of clsuters
-fviz_nbclust(similarity, kmeans, method = "wss")
-
-plot(k.values, wss_values,
-     type="b", pch = 19, frame = FALSE, 
-     xlab="Number of clusters K",
-     ylab="Total within-clusters sum of squares")
-results<- kmeans(similarity, centers=550, nstart = 25)
-table(family$class, results$cluster) # check howmay clusters are matching
-
-
-#Spectral Clust
-# edges connecting different clusters should have low weigths
-S <- similarity
-make.affinity <- function(S, n.neighboors=2) {
-  N <- length(S[,1])
-  
-  if (n.neighboors >= N) {  # fully connected
-    A <- S
-  } else {
-    A <- matrix(rep(0,N^2), ncol=N)
-    for(i in 1:N) { # for each line
-      # only connect to those points with larger similarity 
-      best.similarities <- sort(S[i,], decreasing=TRUE)[1:n.neighboors]
-      for (s in best.similarities) {
-        j <- which(S[i,] == s)
-        A[i,j] <- S[i,j]
-        A[j,i] <- S[i,j] # to make an undirected graph, ie, the matrix becomes symmetric
-      }
-    }
-  }
-  A  
-}
-
-A <- make.affinity(S, 3)
-
-# egree of the respective vertex and all other positions are zero
-
-D <- diag(apply(A, 1, sum)) # sum rows
-D[1:8,1:8]
-
-#unnormalized graph
-U = D-A
-round(U[1:12,1:12],1)
-
-k   <- 500
-evL <- eigen(U, symmetric=TRUE)
-Z   <- evL$vectors[,(ncol(evL$vectors)-k+1):ncol(evL$vectors)]
-plot(Z, col=family$class, pch=20) # notice that all 50 points,
-                                 # of each cluster, are on top of each other
-
-# find the apropoiate clusters
-library(stats)
-km <- kmeans(Z, k)
-library(ggplot2)
-plot(similarity,col=km$cluster, pch=20)
-plot(my.data, col=km$cluster)    
-
-signif(evL$values,2) # eigenvalues are in decreasing order
-plot(rev(evL$values), log="y")
-abline(v=2.25, col="red", lty=2) # there are just 2 clusters as expected
-
-## plot it
-install.packages("yaml")
-install.packages("kknn")
-install.packages("kernlab")
-library(kernlab)
-source("https://bioconductor.org/biocLite.R")
-biocLite("SamSPECTRAL")
-library(SamSPECTRAL)
-set.seed(123)
-
-output <- SamSPECTRAL(similarity, dimension=c(1,2,3),
-                      normal.sigma = 500, separation.factor = 0.6) 
-
-sc <- specc(my.data, centers=2)
-
-plot(similarity, col = sc, centers = 2) # estimated classes (x)
-points(similarity, pch=(29 - 2 * sc)) # true classes (<>)
+my.result <- cbind(proteins)
+my.result <- cbind(my.result, result.kmeans)
+my.result <- cbind(my.result, result.hclust)
+my.result <- cbind(my.result, result.hmm)
+colnames(my.result) <- c("protein", "kmeans", "hclust", "hmm")
 
